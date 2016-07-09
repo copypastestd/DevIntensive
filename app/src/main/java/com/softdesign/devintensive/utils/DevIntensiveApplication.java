0d96e7622668d890e7471ce5cb8765078ce5651f
Timofey Korotkov <copypastestd@gmail.com>
2016-07-10 00:24:58 +0300

    behavior erros

--- app/src/main/java/com/softdesign/devintensive/utils/DevIntensiveApplication.java
+++ app/src/main/java/com/softdesign/devintensive/utils/DevIntensiveApplication.java
@@ -1,6 +1,7 @@
 package com.softdesign.devintensive.utils;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
@@ -8,15 +9,20 @@ public class DevIntensiveApplication extends Application {
 
     public static SharedPreferences sSharedPreferences;
 
+    static Context sContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
-
+        sContext = this;
         sSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-
     }
 
     public static SharedPreferences getSharedPreferences() {
         return sSharedPreferences;
+    }
+
+    public static Context getContext() {
+        return sContext;
     }
 }
