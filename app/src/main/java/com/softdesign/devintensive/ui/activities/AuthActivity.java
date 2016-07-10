40bfb1eee880082fa2443872cc3b57ab328390b2
Timofey Korotkov <copypastestd@gmail.com>
2016-07-11 02:23:27 +0300

    add auth activity

--- app/src/main/java/com/softdesign/devintensive/ui/activities/AuthActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/AuthActivity.java
@@ -1,33 +1,30 @@
 package com.softdesign.devintensive.ui.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import com.softdesign.devintensive.R;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * A login screen activity.
  */
-public class LoginActivity extends BaseActivity {
+public class AuthActivity extends BaseActivity {
 
-    @BindView(R.id.sign_in_btn)
-    Button mSignInBtn;
+    /*@BindView(R.id.sign_in_btn)
+    Button mSignInBtn;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        //setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_auth);
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.sign_in_btn)
+    /*@OnClick(R.id.sign_in_btn)
     public void OnSignInBtnClick() {
         startActivity(new Intent(this, MainActivity.class));
-    }
+    }*/
 }
 
