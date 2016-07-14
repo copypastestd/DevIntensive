c2654926e2f84521440d41505cf79deda5a98276
Timofey Korotkov <copypastestd@gmail.com>
2016-07-14 19:05:24 +0300

    add xml Layout

--- app/src/main/java/com/softdesign/devintensive/data/network/RestService.java
+++ app/src/main/java/com/softdesign/devintensive/data/network/RestService.java
@@ -1,10 +1,12 @@
 package com.softdesign.devintensive.data.network;
 
 import com.softdesign.devintensive.data.network.req.UserLoginReq;
+import com.softdesign.devintensive.data.network.res.UserListRes;
 import com.softdesign.devintensive.data.network.res.UserModelRes;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 /**
@@ -14,5 +16,16 @@ public interface RestService {
 
     @POST("login")
     Call<UserModelRes> loginUser (@Body UserLoginReq req);
+
+    //new
+    /*@Multipart
+    @POST("user/{userId}/publicValues/profilePhoto")
+    Call<UserModelRes> uploadPhoto(@Path("userId") String userId,
+                                   @Part MultipartBody.Part file);*/
+
+    @GET("user/list?orderBy=rating")
+    Call<UserListRes> getUserList();
+
+
 
 }
